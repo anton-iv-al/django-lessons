ef7ba1a04e4e38afa1b26356d690845556fa60b7
anton ivanov <antonival96@gmail.com>
2024-10-17 17:05:24 +0300

    edit user

--- demo_project/auth_app/urls.py
+++ demo_project/auth_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
+from .views.user_view import UserView
 from .views.registration_view import RegistrationView
 from .views.login_view import LoginView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('login/', LoginView.as_view(), name='login'),
+    path('user/', UserView.as_view(), name='user'),
 ]
---
+++ demo_project/auth_app/views/user_view.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest
+from django.shortcuts import render
+from django.views import View
+
+from ..forms.edit_user_form import EditUserForm
+
+USER_TEMPLATE = "user.html"
+
+
+class UserView(LoginRequiredMixin, View):
+    def get(self, request: HttpRequest):
+        form = EditUserForm(instance=request.user)  # type: ignore
+        context = {"edit_user_form": form, "user": request.user}
+        return render(request, USER_TEMPLATE, context)
+
+    def post(self, request: HttpRequest):
+        form = EditUserForm(request.POST, instance=request.user)  # type: ignore
+        if not form.is_valid():
+            context = {"edit_user_form": form, "user": request.user}
+            return render(request, USER_TEMPLATE, context)
+
+        form.save()
+
+        context = {"edit_user_form": form, "user": request.user}
+        return render(request, USER_TEMPLATE, context)
