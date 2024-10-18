b788455babb854b2239f45b3e9ec6b02b2c93691
anton ivanov <antonival96@gmail.com>
2024-10-18 07:50:12 +0300

    profiles and signals (lesson 17)

--- demo_project/auth_app/admin.py
+++ demo_project/auth_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from . import models
+
+
+class ProfileInline(admin.StackedInline):
+    model = models.Profile
+
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserProfileAdmin(UserAdmin):
+    inlines = (ProfileInline,)
--- demo_project/auth_app/models.py
+++ demo_project/auth_app/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    avatar = models.ImageField(null=True, blank=True)
+    phone = models.CharField(
+        validators=[RegexValidator(r"^\+?[1-9][0-9]{7,14}$")],
+        max_length=17,
+        null=True,
+        blank=True,
+    )
+    about = models.TextField(null=True, blank=True)
+    github_link = models.URLField(null=True, blank=True)
