24cd5bb825d5e7876a3cab15f63a58431aabb5ba
anton ivanov <antonival96@gmail.com>
2024-10-24 02:12:42 +0300

    tags optimization

--- demo_project/post_tags_app/signals.py
+++ demo_project/post_tags_app/signals.py
@@ -2,8 +2,8 @@ import re
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from posts_app.models import Post
+
 from .models import PostTag
 
 hashtags_pattern = re.compile(r"(?<=#)\w+")
@@ -11,24 +11,25 @@ hashtags_pattern = re.compile(r"(?<=#)\w+")
 
 @receiver(post_save, sender=Post)
 def add_post_tags(sender, instance: Post, created, *args, **kwargs):
-    hashtags = hashtags_pattern.findall(instance.text)
+    hashtags = set(hashtags_pattern.findall(instance.text))
 
-    old_tags = set(instance.tags.all())
+    old_tags = {tag.name: tag for tag in instance.tags.all()}
 
-    new_tags = set()
-    for tag_name in hashtags:
+    for tag_name in hashtags - old_tags.keys():
         if len(tag_name) > PostTag._meta.get_field("name").max_length:
             continue
 
-        tag, _ = PostTag.objects.get_or_create(name=tag_name, defaults={"count": 0})
-        new_tags.add(tag)
-
-    for tag in new_tags - old_tags:
+        tag, created = PostTag.objects.get_or_create(
+            name=tag_name,
+            defaults={"count": 1},
+        )
+        if not created:
+            tag.count += 1
+            tag.save()
         instance.tags.add(tag)
-        tag.count += 1
-        tag.save()
 
-    for tag in old_tags - new_tags:
+    for tag_name in old_tags.keys() - hashtags:
+        tag = old_tags[tag_name]
         instance.tags.remove(tag)
         tag.count -= 1
         if tag.count == 0:
